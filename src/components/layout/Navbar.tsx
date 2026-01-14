78b5ba786c669afc45794f9098e9f319b46502f3
uttam <uttamgupta628@gmail.com>
2026-01-14 10:58:02 +0530

    login final

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Menu, X, LogOut, User, ChevronDown } from "lucide-react";
 import { useAuth } from '../../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
